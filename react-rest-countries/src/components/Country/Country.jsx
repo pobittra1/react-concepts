91db2ad3196b9f50ea4c5128876b3f243c610199
pobittra chandra roy <devpobittra@gmail.com>
2026-02-25 12:05:16 +0600

    render conditionally button click data and css

--- react-rest-countries/src/components/Country/Country.jsx
+++ react-rest-countries/src/components/Country/Country.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import "./Country.css"
 
 const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false);
+    const handleVisit = () => {
+        // normal method to toggle state
+        // if (visited) {
+        //     setVisited(false);
+        // } else {
+        //     setVisited(true);
+        // }
+        //using ternary
+        // visited ? setVisited(false) : setVisited(true);
+        //using more shortcut
+        //if visited true so !Visited mean set opposite of true. if false so !Visited mean set opposite of false is true.
+        setVisited(!visited);
+    }
     console.log(country);
     return (
-        <div className="country">
+        <div className={`country ${visited && "visited-country"}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Country name: {country.name.common}</h3>
             <h4>Area of <span className="highlightCount">{country.name.common}</span> is {country.area.area}</h4>
             {/* conditional rendering here */}
             <p>population is: {country.population.population}. {country.population.population > 3000000 ? "Over populated country" : "best Country"}</p>
+            <button onClick={handleVisit}>{visited ? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
