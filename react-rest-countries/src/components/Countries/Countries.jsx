416e260da0e799506f180db4727d6636ca79f0e6
pobittra chandra roy <devpobittra@gmail.com>
2026-02-25 15:36:32 +0600

    use props drilling, handle country visited from upper component

--- react-rest-countries/src/components/Countries/Countries.jsx
+++ react-rest-countries/src/components/Countries/Countries.jsx
@@ -1,8 +1,14 @@
-import { use } from "react";
+import { use, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css"
 
 const Countries = ({ countriesPromise }) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleaVisitedCountries = (country) => {
+        console.log(country);
+    }
+
     //resolve the countriesPromise here using use() hook
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
@@ -12,7 +18,10 @@ const Countries = ({ countriesPromise }) => {
             <h1>Countries are Here...</h1>
             <div className="countriesContainer">
                 {
-                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                    countries.map(country => <Country
+                        key={country.cca3.cca3}
+                        country={country}
+                        handleaVisitedCountries={handleaVisitedCountries}></Country>)
                 }
             </div>
         </div>
--- react-rest-countries/src/components/Country/Country.jsx
+++ react-rest-countries/src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Country.css"
 
-const Country = ({ country }) => {
+const Country = ({ country, handleaVisitedCountries }) => {
     const [visited, setVisited] = useState(false);
     const handleVisit = () => {
         // normal method to toggle state
@@ -15,8 +15,9 @@ const Country = ({ country }) => {
         //using more shortcut
         //if visited true so !Visited mean set opposite of true. if false so !Visited mean set opposite of false is true.
         setVisited(!visited);
+        handleaVisitedCountries(country);
     }
-    console.log(country);
+
     return (
         <div className={`country ${visited && "visited-country"}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
